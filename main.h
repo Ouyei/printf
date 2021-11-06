aaef7402b895794942cd2704212bd62364d205a2
Ouyei <obedat@me.com>
2021-11-06 08:57:22 -0700

    added: header
    Co-authored-by: Bruno Rodríguez  <40237383+brunonra-dev@users.noreply.github.com>

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+/**
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+
+typedef struct specifiers
+{
+	char *specifiers;
+	char (*f)(char s);
+}specifiers_t;
+
+int _printf(const char *format, ...);
+int eval_format(const char *c, ...);
+
+#endif
